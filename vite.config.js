3af5490644002a30724073c4b5aacef8bde3925e
Zach Johnson <zachjohnson@Zachs-MBP-2.attlocal.net>
2024-07-26 14:01:41 -0500

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "Zach-Yates/portfolio-nft-wallet-viewer/"
+  base: "/portfolio-nft-wallet-viewer/"
 })
